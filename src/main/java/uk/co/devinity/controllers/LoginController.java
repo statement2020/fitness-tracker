b5c30f2060b3f8c7ee41c0c6116760aef9fe7337
Jamie Bowdidge <jamie@Jamies-MacBook-Pro.local>
2025-09-01 16:48:34 +0100

    Enable registration flow
    * Registration
    * Requires admin approval

--- src/main/java/uk/co/devinity/controllers/LoginController.java
+++ src/main/java/uk/co/devinity/controllers/LoginController.java
@@ -1,13 +1,17 @@
 package uk.co.devinity.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
     public String login() {
-        return "login"; // Thymeleaf template login.html
+        return "login";
     }
 }
